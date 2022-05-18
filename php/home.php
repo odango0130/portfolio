c1904b2ca528b3cd1a6a28685d31b97ab8568eb2
odango0130 <92291544+odango0130@users.noreply.github.com>
2022-05-19 04:15:58 +0900

    First commit

---
+++ php/home.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="ポートフォリオ">
+    <link rel="stylesheet" href="../css/css_home.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
